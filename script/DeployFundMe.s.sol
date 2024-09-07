68f08158737f35a15cdc05e0d100282bfd88713f
shahram <shahram.shahdadi@gmail.com>
2024-09-07 12:28:01 +0200

    Lesson 10 mock

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
 import {FundMe} from "../src/FundMe.sol";
 import {Script} from "forge-std/Script.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     AggregatorV3Interface priceFeed;
 
+    HelperConfig helperConfig = new HelperConfig();
+    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
     function run() external returns (FundMe) {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
